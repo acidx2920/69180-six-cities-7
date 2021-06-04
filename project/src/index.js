8818b64c2b2fec7df2850fb734507f285011ba8c
acx <acidx2920@gmail.com>
2021-06-04 11:55:12 +0300

    добавляет первые компоненты

--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const config = {
+  proposalsNum: 312,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App proposalsNum={config.proposalsNum} />
   </React.StrictMode>,
   document.getElementById('root'));
