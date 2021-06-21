0b3043fc3a062e369fe5e9bc5db7086496cf506f
acx <acidx2920@gmail.com>
2021-06-21 18:17:13 +0300

    добавляет компонент RadioButton

---
+++ project/src/components/radio-button/radio-button.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function RadioButton(props) {
+  const {value, title, checked, handleChange} = props;
+
+  return (
+    <>
+      <input className="form__rating-input visually-hidden" name="rating" value={value} id={`${value}-stars`} type="radio" checked={checked} onChange={handleChange} />
+      <label htmlFor={`${value}-stars`} className="reviews__rating-label form__rating-label" title={title}>
+        <svg className="form__star-image" width="37" height="33">
+          <use xlinkHref="#icon-star" />
+        </svg>
+      </label>
+    </>
+  );
+}
+
+RadioButton.propTypes = {
+  value: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  checked: PropTypes.bool.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default RadioButton;
